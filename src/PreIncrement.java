1e742b40e8980c017d18b3ebb87a7ccbd5b771b6
Matheus Alves <matheusalvessilvabr@gmail.com>
2025-06-17 10:51:52 -0300

    adding preincrement

---
+++ src/PreIncrement.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+
+public class PreIncrement extends Main {
+    // pre-increment ++i Will increment our value of i by 1 and then use it in our statement.
+    public PrintStream preIncrementShow(){
+        int integerNumber = 10;
+        int preincrementNumber = ++integerNumber;
+
+        return System.out.printf(String.valueOf(preincrementNumber));
+    }
+}
