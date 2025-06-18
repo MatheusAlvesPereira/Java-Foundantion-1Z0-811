8e7f066fe655187a289e091aa0a461842e774b30
Matheus Alves <matheusalvessilvabr@gmail.com>
2025-06-18 09:45:30 -0300

    adding preincrement class Closes #1

--- src/PreIncrement.java
+++ src/PreIncrement.java
@@ -1,6 +1,6 @@
 import java.io.PrintStream;
 
-public class PreIncrement extends Main {
+public class PreIncrement {
     // pre-increment ++i Will increment our value of i by 1 and then use it in our statement.
     public PrintStream preIncrementShow(){
         int integerNumber = 10;
