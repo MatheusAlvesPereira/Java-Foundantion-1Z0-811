5bad47f465c04297361645d3102679e79cb67504
Matheus Alves <matheusalvessilvabr@gmail.com>
2025-06-21 14:07:44 -0300

    adding arraylist class

--- src/Arraylist.java
+++ src/Arraylist.java
@@ -1,5 +1,6 @@
-// 1. Write a Java program to create a new array list, add some elements (string) and print out the collection
+//You should remember how to compare and contrast Arrays and ArrayLists.
+// Also understand both their behaviors when elements are added, removed, and updated.
 
 public class Arraylist {
-
+    String[] fruitsArray = new String[4];
 }
